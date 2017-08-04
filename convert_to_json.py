70972485561e0a79ebd128e7f1d9056305f4f565
yaskyj <yaskyj@yahoo.com>
2017-08-03 22:50:43 -0500

    Complete data problems and complete comments on files

--- convert_to_json.py
+++ convert_to_json.py
@@ -6,12 +6,14 @@ import re
 import codecs
 import json
 
+#Regex for names with one colon
 lower_colon = re.compile(r'^([a-z]|_)*:([a-z]|_)*$')
+#Regex for names with characters that will be ignored
 problemchars = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
+#Regex for the end of street names
 street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
 
-
-#Based on review of audit_street_names script
+#Mapping based on review of audit_street_names script
 street_mapping = { "St": "Street",
             "St.": "Street",
             "Ave": "Avenue",
@@ -24,18 +26,26 @@ street_mapping = { "St": "Street",
             "Ave.": "Avenue"
             }
 
-#Based on review of explor_tag_types script
+#Mapping based on review of explor_tag_types script
 city_mapping = { "Boulder, CO": "Boulder",
             "lafayette": "Lafayette",
             " Lafayette": "Lafayette",
             u"Boulder, CO \u200e": "Boulder",
-            "CO": "Boulder",
             "boulder": "Boulder"
             }
 
+#Mapping based on review of explor_tag_types script
+post_mapping = { "80026-2872": "80026",
+            "80303-1229": "80303",
+            "80305-9998": "80305",
+            "CO 80027": "80027",
+            "CO 80305": "80305"
+            }
+
+#List for the created portion of the item's dictionary
 CREATED = [ "version", "changeset", "timestamp", "user", "uid"]
 
-
+#Checks for the street names ending with a key in the street_mapping, returns the proper name if found
 def clean_street_name(street_name):
     m = street_type_re.search(street_name)
     if m:
@@ -45,68 +55,105 @@ def clean_street_name(street_name):
         else:
             return street_name
 
+#Checks if tag is the street
 def is_street_name(elem):
     return elem.attrib['k'] == "addr:street"
 
+#Checks for the city names in the key of city_mapping, returns the proper name if found
 def clean_city_name(city_name):
     if city_name in city_mapping:
         return city_mapping[city_name]
     else:
         return city_name
 
+#Check if the tag is the city
 def is_city_name(elem):
     return elem.attrib['k'] == "addr:city"
 
+#Checks for the city names in the key of city_mapping, returns the proper name if found
+def clean_post(post):
+    if post in post_mapping:
+        return post_mapping[post]
+    else:
+        return post
+
+#Check if the tag is the city
+def is_post(elem):
+    return elem.attrib['k'] == "addr:postcode"
+
 def shape_element(element):
+
     node = {}
+
+    #Only adding tags that are node or way
     if element.tag == "node" or element.tag == "way" :
         node["type"] = element.tag
 
+        #Adds all node references to one list
         if element.tag == "way":
             node["node_refs"] = []
             for nd in element.iter("nd"):
                 node["node_refs"].append(nd.attrib['ref'])
+
         #Create pos list to hold latitude and longitude
         pos = []
 
         #Iterate through the element attributes
         for attribute in element.attrib:
-            #Check to see if the attribute in in the created list
+
+            #Check to see if the attribute is in the created list
             if attribute in CREATED:
+                #Checks if created already exists in node, if not add it before adding
+                #the attribute
                 if "created" in node:
                     node["created"][attribute] = element.get(attribute)
                 else:
                     node["created"] = {}
                     node["created"][attribute] = element.get(attribute)
+
             #If latitude or longitude then insert into the pos list
             elif attribute == "lat":
                 pos.insert(0, float(element.get(attribute)))
             elif attribute == "lon":
                 pos.insert(-1, float(element.get(attribute)))
-            #Other attributes are just added
+
+            #Other attributes are just added with colons replaces with underscores
             else:
                 node[attribute.replace(":", "_")] = element.get(attribute) 
 
-        #If pos exists, then add
+        #If pos exists, then add to node
         if len(pos) > 0:
             node["pos"] = pos
 
         #Iterate through tag elements
         for tag in element.iter("tag"):
+
             #Check for problem characters and ignore if present
             if problemchars.match(tag.attrib['k']):
                 pass
-            #Check if the tag more than one colon and if so, ignore
+
+            #Check if the tag more than one colon and if so, ignores tag
             elif tag.attrib['k'].count(":") > 1:
                 pass
-            #Match against one colon matches
+
+            #Match against one colon
             elif lower_colon.match(tag.attrib['k'].lower()):
+
+                #Checks for address attributes
                 if tag.attrib['k'].find("addr:") == 0:
+                    #Checks if address is already in node, if not, the else
+                    #statement creates it before adding the attribute
                     if "address" in node:
+                        #Checks for street name, cleans, and adds
                         if is_street_name(tag):
                             node["address"]["street"] = clean_street_name(tag.attrib['v'])
+                        #Checks for city name, clean, and adds
                         elif is_city_name(tag):
                             node["address"]["city"] = clean_city_name(tag.attrib['v'])
+                        elif is_post(tag):
+                            node["address"]["postcode"] = clean_post(tag.attrib['v'])
+                        #Takes the second part of the address and adds that and the
+                        #value to the node
                         else:
                             node["address"][tag.attrib['k'].split(":")[1]] =  tag.attrib['v']
                     else:
@@ -116,11 +163,17 @@ def shape_element(element):
                         elif is_city_name(tag):
                             node["address"] = {}
                             node["address"]["city"] = clean_city_name(tag.attrib['v'])
+                        elif is_post(tag):
+                            node["address"] = {}
+                            node["address"]["postcode"] = clean_post(tag.attrib['v'])
                         else:
                             node["address"] = {}
                             node["address"][tag.attrib['k'].split(":")[1]] =  tag.attrib['v']
+
+                #All other one colon tags have the colon replaced with an underscore
                 else:
                     node[tag.attrib['k'].replace(":", "_")] = tag.attrib['v']
+            #Adds normal node
             else:
                 node[tag.attrib['k']] = tag.attrib['v']
 
--- explore_tag_types.py
+++ explore_tag_types.py
@@ -12,10 +12,12 @@ osm_file = open("boulder.osm", "r")
 tag_counts = defaultdict(int)
 tag_values = defaultdict(set)
 
+#Adds counts and values of tags to the two dictionaries above
 def explore_tags(tag_type, tag_value):
     tag_counts[tag_type] += 1
     tag_values[tag_type].add(tag_value)
 
+#Prints the dictionary containing sets by the key
 def print_sorted_dict(d):
     keys = d.keys()
     keys = sorted(keys, key=lambda s: s.lower())
@@ -23,20 +25,25 @@ def print_sorted_dict(d):
         v = d[k]
         print "%s: %d" % (k, v) 
 
+#Prints the tags by descending total for each
 def print_sorted_amounts(d):
     values = sorted(d.items(), key=lambda x:x[1], reverse=True)
     for k,v in values:
         print "%s: %d" % (k, v) 
 
+#Takes the dictionary with total for each tag, sorts them by amounts
+#then adds the top X number using the count variable to a new dictionary 
+#in order to review those values
 def print_top_tags(d):
     count = 0
     top_tags = {}
     values = sorted(d.items(), key=lambda x:x[1], reverse=True)
     for k,v in values:
-        if count < 10:
-            # if k != "addr:street" and k != "name":
-            top_tags[k] = tag_values[k]
-            count += 1
+        if count < 6:
+            #The if statement can be used to filter out already reviewed tag types
+            if k != "tiger:cfcc" and k != "tiger:county" and k != "natural" and k != "name" and k != "highway" and k != "addr:street":
+                top_tags[k] = tag_values[k]
+                count += 1
         else:
             break
     pprint.pprint(dict(top_tags))
@@ -45,8 +52,9 @@ def audit():
     for event, elem in ET.iterparse(osm_file):
         for tag in elem.iter("tag"):
             explore_tags(tag.attrib['k'], tag.attrib['v'])
+
+    #Different print statements to show different tag analyses
     # pprint.pprint(dict(tag_counts))
-    # print len(tag_counts)
     # print_sorted_dict(tag_counts)
     # print_sorted_amounts(tag_counts)
     print_top_tags(tag_counts)
